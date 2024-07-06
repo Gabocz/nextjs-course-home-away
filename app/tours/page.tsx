51be7469ef8b0290af2d6442683fc855adc638a3
Gabocz <gabriella.ando@gmail.com>
2024-07-06 21:04:45 +0200

    add dynamic page

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,4 +1,5 @@
 const url = "https://www.course-api.com/react-tours-project";
+import Link from "next/link";
 
 type Tour = {
   id: string;
@@ -9,7 +10,6 @@ type Tour = {
 };
 
 const fetchTours = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(url);
   const data: Tour[] = await response.json();
   return data;
@@ -20,9 +20,17 @@ async function ToursPage() {
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map((tour) => (
-        <h2 key={tour.id}>{tour.name}</h2>
-      ))}
+      {data.map((tour) => {
+        return (
+          <Link
+            key={tour.id}
+            href={`/tours/${tour.id}`}
+            className="hover:text-blue-500"
+          >
+            <h2>{tour.name}</h2>
+          </Link>
+        );
+      })}
     </section>
   );
 }
