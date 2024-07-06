3371b7a250a21565598ec24736c6cd79c5127f60
Gabocz <gabriella.ando@gmail.com>
2024-07-06 18:39:12 +0200

    add navbar, tours page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,17 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+// const inconsolata = Inconsolata({ subsets: ["latin"] });
+// const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
+
+export const metadata: Metadata = {
+  title: "Next.js Project",
+  description: "A Next.js project with TypeScript and TailwindCSS.",
+  keywords: "Next.js, Typescript, TailwindCSS",
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <nav>hello there</nav>
-        {children}
+      <body className={inter.className}>
+        <Navbar />
+        <main className="max-w-3xl mx-auto py-10">{children}</main>
       </body>
     </html>
   );
